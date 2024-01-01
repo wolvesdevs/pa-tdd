d935e7a0faf1c665da2b52ca20e4865d45abc55b
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2024-01-01 20:42:42 +0900

    refs #03 テスト駆動でテストを書く

--- src/TDD/TDD.UI/Calculation.cs
+++ src/TDD/TDD.UI/Calculation.cs
@@ -1,7 +1,15 @@
-﻿namespace TDD.UI
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDD.UI
 {
     public static class Calculation
     {
+        public static int Ave(List<int> list)
+        {
+            throw new NotImplementedException();
+        }
+
         public static int Sum(int a, int b)
         {
             return a + b;
--- src/TDD/TDDTest.Tests/UnitTest1.cs
+++ src/TDD/TDDTest.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace TDDTest.Tests
 {
@@ -11,6 +12,14 @@ namespace TDDTest.Tests
         {
             var val = TDD.UI.Calculation.Sum(2, 5);
             Assert.AreEqual(7, val);
+        }
+
+        [TestMethod]
+        public void 平均値を取得できる()
+        {
+            var list = new List<int> { 1, 2, 3, 4 , 5 };
+            var result = TDD.UI.Calculation.Ave(list);
+            Assert.AreEqual(3, result);
         }
     }
 }
