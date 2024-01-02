c291c099bde2975e13d3ad96410146bf6c65654b
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2024-01-02 16:54:08 +0900

    refs #12 IPropertyChanged

--- src/TDD/TDD.UI/Form1ViewModel.cs
+++ src/TDD/TDD.UI/Form1ViewModel.cs
@@ -1,14 +1,69 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace TDD.UI
 {
-    public sealed class Form1ViewModel
+    public sealed class Form1ViewModel : INotifyPropertyChanged
     {
         //private System.Windows.Forms.Button CalculationButton;
 
-        public string ATextBoxText { get; set; } = string.Empty;
-        public string BTextBoxText { get; set; } = string.Empty;
-        public string ResultLabelText { get; set; } = string.Empty;
+        private string _aTextBoxText = string.Empty;
+        public string ATextBoxText
+        {
+            get { return _aTextBoxText; }
+            set
+            {
+                if (_aTextBoxText == value)
+                {
+                    return;
+                }
+
+                _aTextBoxText = value;
+                OnPropertyChanged("ATextBoxText");
+            }
+        }
+
+        private string _bTextBoxText = string.Empty;
+        public string BTextBoxText
+        {
+            get { return _bTextBoxText; }
+            set
+            {
+                if (_bTextBoxText == value)
+                {
+                    return;
+                }
+
+                _bTextBoxText = value;
+                OnPropertyChanged("BTextBoxText");
+            }
+        }
+
+        private string _resultLabelText = string.Empty;
+        public string ResultLabelText
+        {
+            get { return _resultLabelText; }
+            set
+            {
+                if (_resultLabelText == value)
+                {
+                    return;
+                }
+
+                _resultLabelText = value;
+                OnPropertyChanged("ResultLabelText");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
 
         public void CalculationAction()
         {
