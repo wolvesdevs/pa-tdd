721fd6dadd7c3d98aea7277d2627e336b2ee262f
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2024-01-02 16:20:40 +0900

    refs #11 データバインド

--- src/TDD/TDD.UI/Form1View.cs
+++ src/TDD/TDD.UI/Form1View.cs
@@ -18,6 +18,10 @@ namespace TDD.UI
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
+
+            ATextBox.DataBindings.Add("Text", _viewModel, "ATextBoxText");
+            BTextBox.DataBindings.Add("Text", _viewModel, "BTextBoxText");
+            ResultLabel.DataBindings.Add("Text", _viewModel, "ResultLabelText");
         }
 
         private void CalculationButton_Click(object sender, EventArgs e)
