63f4d8f5fb801cff7fd38453031610945c4a9b81
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2024-01-03 11:48:06 +0900

    refs #19 Chaining Assertionの使い方

---
+++ src/UsefulTool/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UsefulTool;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, Class1.Add(1, 2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void 例外のテスト()
+        {
+            Assert.AreEqual(3, Class1.Add(-1, 2));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Class1.Add(1,3).Is(4);
+            var ex = AssertEx.Throws<InputException>(() => Class1.Add(-1, 3));
+            ex.Message.Is("マイナス値は入力できません");
+        }
+    }
+}
---
+++ src/UsefulTool/UsefulTool/InputException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UsefulTool
+{
+    public sealed class InputException : Exception
+    {
+        public InputException(string message) : base(message)
+        {
+        }
+    }
+}
