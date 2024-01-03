aa91adb584c0b5bd2030768b417bed7cb0f366c9
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2024-01-03 14:08:41 +0900

    refs #20 カバレッジの意味と採り方

---
+++ src/TestCoverage/TestCoverage/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace TestCoverage
+{
+    public class Class1
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Add2(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ src/TestCoverage/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestCoverage;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, Class1.Add(1, 2));
+        }
+    }
+}
